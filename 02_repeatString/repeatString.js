52f7208191ed5218f6709ee3eca6e33170118e45
Unmystic <unmystic7@gmail.com>
2025-02-02 15:07:09 +0100

    01/02 ex done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function (value, num) {
+  let output = "";
+  if (num < 0) {
+    return "ERROR";
+  }
+  for (let i = 0; i < num; i++) output += value;
+  return output;
 };
 
 // Do not edit below this line
