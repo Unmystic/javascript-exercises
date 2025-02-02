3a1315e0230069b4e31d6c7915e3c8a879a6c1fb
Unmystic <unmystic7@gmail.com>
2025-02-02 16:56:03 +0100

    Solved till 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,21 @@
-const sumAll = function() {
-
+const sumAll = function (val1, val2) {
+  // Better to use isInteger method
+  if (
+    val1 < 0 ||
+    val2 < 0 ||
+    typeof val1 != "number" ||
+    typeof val2 != "number" ||
+    val1 % 1 != 0 ||
+    val2 % 1 != 0
+  )
+    return "ERROR";
+  const minVal = val1 < val2 ? val1 : val2;
+  const maxVal = val1 > val2 ? val1 : val2;
+  let sumsAll = 0;
+  for (let i = minVal; i <= maxVal; i++) {
+    sumsAll += i;
+  }
+  return sumsAll;
 };
 
 // Do not edit below this line
