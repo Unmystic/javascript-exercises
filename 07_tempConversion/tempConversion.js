acb72afb1dcf57abbd9034d633ea41b30621b730
Unmystic <unmystic7@gmail.com>
2025-02-02 17:22:00 +0100

    Solved till 7(incl)

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (value) {
+  // if (!Number.isFinite(value)) return "Error";
+  let valueC = ((value - 32) * 5) / 9;
+  // Rounds up to 1 decimal places
+  return Math.ceil(valueC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (value) {
+  // if (!Number.isFinite(value)) return "Error";
+  let valueF = (value * 9) / 5 + 32;
+  return Math.ceil(valueF * 10) / 10;
 };
 
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
