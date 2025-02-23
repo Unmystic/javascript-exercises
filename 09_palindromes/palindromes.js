2c8b2022513cae7eb444cbb099c1cbb87001cd8f
Unmystic <unmystic7@gmail.com>
2025-02-23 17:29:28 +0100

    solved 9 using 2 pointers

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,23 @@
-const palindromes = function () {
+const isAlphaNumeric = (str) => /^[a-z0-9]*$/gi.test(str);
 
+const palindromes = function(s) {
+    s = s.toLowerCase()
+    let i = 0;
+    let j = s.length - 1;
+    while (j > i) {
+        if (!isAlphaNumeric(s[j])) {
+            j--;
+            continue;
+        }
+        if (!isAlphaNumeric(s[i])) {
+            j--;
+            continue;
+        }
+        if (s[i] != s[j]) return false;
+        i++
+        j--
+    }
+    return true;
 };
 
 // Do not edit below this line
